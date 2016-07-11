28b178d7721469ca9495b5dad2ddd3fe3d96fb37
Chang Wang <garnwraly@gmail.com>
2016-07-11 16:01:03 -0400

    Webpack stub. compiles

---
+++ dcc-portal-ui/app/scripts/vendor.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// bower modules
+require('script!jquery/dist/jquery.js');
+require('script!lodash/lodash.js');
+require('script!angularjs/angular.js');
+require('script!angular-sanitize/angular-sanitize.js');
+require('script!angular-animate/angular-animate.js');
+require('script!angular-cookies/angular-cookies.js');
+require('script!angular-loading-bar/build/loading-bar.js');
+require('script!restangular/dist/restangular.js');
+require('script!angular-ui-router/release/angular-ui-router.js');
+require('script!ngInfiniteScroll/build/ng-infinite-scroll.js');
+require('script!angular-local-storage/dist/angular-local-storage.js');
+require('script!angularytics/dist/angularytics.js');
+require('script!angular-ui-utils/scrollfix.js');
+require('script!angular-markdown-directive/markdown.js');
+require('script!angular-lodash/angular-lodash.js');
+require('script!angular-bootstrap/ui-bootstrap-tpls.min.js');
+require('script!angularjs-toaster/toaster.js');
+require('script!zeroclipboard/dist/ZeroClipboard.js');
+require('script!highcharts/highcharts.js');
+require('script!highcharts/modules/no-data-to-display.src.js');
+require('script!d3/d3.min.js');
+require('script!Blob/Blob.js');
+require('script!FileSaver/FileSaver.min.js');
+require('script!showdown/src/showdown.js');
+require('script!showdown/src/extensions/table.js');
+require('script!x2js/xml2json.js');
+
+
+require('script!./ui/js/table2CSV.js');
+require('script!../vendor/scripts/invariant.js');
+
+// BAM iobio
+require('script!../vendor/scripts/bamiobio/class.js');
+require('script!../vendor/scripts/bamiobio/rdp.js');
+require('script!../vendor/scripts/bamiobio/queue.min.js');
+require('script!../vendor/scripts/bamiobio/donut.d3.js');
+require('script!../vendor/scripts/bamiobio/histogram.d3.js');
+require('script!../vendor/scripts/bamiobio/histogramViewFinder.d3.js');
+require('script!../vendor/scripts/bamiobio/movingLine.d3.js');
+require('script!../vendor/scripts/bamiobio/bam.iobio.js/bam.iobio.js');
+require('script!../vendor/scripts/bamiobio/bam.iobio.js/bam.js');
+require('script!../vendor/scripts/bamiobio/bam.iobio.js/bin.js');
+require('script!../vendor/scripts/bamiobio/binary.js');
+require('script!../vendor/scripts/bamiobio/flatui-checkbox.js');
+
+// VCF iobio
+require('script!../vendor/scripts/vcfiobio/vcf.iobio.js');
+require('script!../vendor/scripts/vcfiobio/histogram.d3.vcf.js');
+require('script!../vendor/scripts/vcfiobio/barChartAlt.d3.js');
+require('script!../vendor/scripts/vcfiobio/donutChooser.d3.js');
+require('script!../vendor/scripts/vcfiobio/groupedBar.d3.js');
+require('script!../vendor/scripts/vcfiobio/line.d3.js');
+
+// Genome Viewer
+require('script!../vendor/scripts/backbone.min-1.0.js');
+require('script!../vendor/scripts/bootstrap.min.js');
+require('script!../vendor/scripts/bootstrap-slider-2.0.0.js');
+require('script!../vendor/scripts/genome-viewer/vendor/qtip2/jquery.qtip.min.js');
+require('script!../vendor/scripts/genome-viewer/vendor/uri.js/src/URI.min.js');
+require('script!../vendor/scripts/genome-viewer/gv-config.js');
+require('script!../vendor/scripts/genome-viewer/genome-viewer.js');
+
+// Highcharts Rounded Corners
+require('script!../vendor/scripts/highcharts-rounded-corners.js');
+
+// Custom ICGC Genome Viewer Components
+require('script!../vendor/scripts/genome-viewer/icgc-gene-adapter.js');
+require('script!../vendor/scripts/genome-viewer/icgc-gene-track.js');
+require('script!../vendor/scripts/genome-viewer/icgc-mutation-adapter.js');
+require('script!../vendor/scripts/genome-viewer/icgc-mutation-track.js');
+require('script!../vendor/scripts/genome-viewer/icgc-navigation-bar.js');
