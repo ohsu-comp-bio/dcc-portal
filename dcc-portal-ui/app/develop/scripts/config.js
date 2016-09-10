bb72e85fd0743fcd1fceef32ea5135fa0f31d82d
Brian Walsh <brian@bwalsh.com>
2016-09-09 23:27:31 -0700

    ohsu specific branding

--- dcc-portal-ui/app/develop/scripts/config.js
+++ dcc-portal-ui/app/develop/scripts/config.js
@@ -1,9 +1,9 @@
 // Global $ICGC_DEV_CONFIG used for dev environment
 // profile
 var $ICGC_DEV_CONFIG = {
-	// Set DEVELOPER_LOCAL_MODE to true to turn on local developer profile 
+	// Set DEVELOPER_LOCAL_MODE to true to turn on local developer profile
 	// for the 'grunt server' task
-	DEVELOPER_LOCAL_MODE: true, 
-	API_PORT: 9000,
-	HOST: 'local.dcc.icgc.org'
+	DEVELOPER_LOCAL_MODE: true,
+	API_PORT: 443,
+	HOST: 'dcc.icgc.org'
 };
